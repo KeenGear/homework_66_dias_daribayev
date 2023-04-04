7d4996c322a15fc8ac0350832d561f154967cc0f
Dias Daribayev <fission.frames-0s@icloud.com>
2023-04-04 11:37:21 +0600

    Second task commit

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,13 @@ const form3 = document.getElementById('string-2')
 const form4 = document.getElementById('string-4')
 const form5 = document.getElementById('string-1')
 console.log(form1, form2, form3, form4, form5)
+
+
+const elements = document.querySelectorAll(".element");
+
+elements[0].style.color = "red";
+elements[1].style.color = "red";
+elements[2].style.color = "red";
+elements[3].style.color = "green";
+elements[4].style.color = "green";
+elements[5].style.color = "green";
