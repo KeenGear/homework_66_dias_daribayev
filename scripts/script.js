cb28ac77c315e99a45c857ce98b009432f06c075
Dias Daribayev <fission.frames-0s@icloud.com>
2023-04-04 11:32:02 +0600

    First task commit

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const form1 = document.getElementById('string-3')
+const form2 = document.getElementById('string-5')
+const form3 = document.getElementById('string-2')
+const form4 = document.getElementById('string-4')
+const form5 = document.getElementById('string-1')
+console.log(form1, form2, form3, form4, form5)
